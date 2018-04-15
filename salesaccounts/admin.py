eea817aca8befa9bd706f60cd4b46b3451bb843d
Tetyana Loskutova <tetyana.loskutova@gmail.com>
2018-04-15 14:01:32 +0200

    added autopopulate user
    added autopopulate user

--- salesaccounts/admin.py
+++ salesaccounts/admin.py
@@ -4,13 +4,59 @@ from simple_history.admin import SimpleHistoryAdmin
 
 
 class SalesLeadHistoryAdmin(SimpleHistoryAdmin):
-    list_display = ["CRM_id", "history"]
-    history_list_display = ["sales_narrative"]
-    search_fields = ['CRM_id', 'user__username']
+	list_display = ["owner", "account", "Probability", "description", "next_action", "next_action_date"]
+	history_list_display = [ "next_action", "next_action_date", "user"]
+	search_fields = ['CRM_id', "owner", "account", 'est_decision_date', "Probability", "description", "next_action"]
+	list_filter = ['owner', 'est_decision_date', 'status', "next_action_date"]
+	ordering = ('-owner',)
+	readonly_fields = ('user_account',)
 	
-admin.site.register(Account, SimpleHistoryAdmin)
-admin.site.register(CREmployee, SimpleHistoryAdmin)
-admin.site.register(ServiceType, SimpleHistoryAdmin)
+	def save_model(self, request, obj, form, change):
+		obj.user_account = request.user
+		obj.save()
+
+	
+class AccountGroupAdmin(SimpleHistoryAdmin):	
+	list_display = ('name',)
+	readonly_fields = ('user_account',)
+	history_list_display = ( "user_account",'name')
+	
+	def save_model(self, request, obj, form, change):
+		obj.user_account = request.user
+		obj.save()
+
+class AccountAdmin(SimpleHistoryAdmin):	
+	list_display = ['account_group', 'name', 'sector', 'relationship_status', 'region', 'account_manager', 'date_created']
+	readonly_fields = ('user_account',)
+	history_list_display = ( "relationship_status",'account_manager')
+	
+	def save_model(self, request, obj, form, change):
+		obj.user_account = request.user
+		obj.save()
+
+class CREmployeeAdmin(SimpleHistoryAdmin):	
+	list_display = ['name', 'short_name']
+	readonly_fields = ('user_account',)
+	history_list_display = ( "name",'short_name')
+	
+	def save_model(self, request, obj, form, change):
+		obj.user_account = request.user
+		obj.save()
+		
+class ServiceTypeAdmin(SimpleHistoryAdmin):	
+	list_display = ('service_type','service_name')
+	readonly_fields = ('user_account',)
+	history_list_display = ( "user_account",'service_name')
+	
+	def save_model(self, request, obj, form, change):
+		obj.user_account = request.user
+		obj.save()
+
+		
+admin.site.register(Account, AccountAdmin)
+admin.site.register(CREmployee, CREmployeeAdmin)
+admin.site.register(ServiceType, ServiceTypeAdmin)
 
 admin.site.register(SalesLead, SalesLeadHistoryAdmin)
+admin.site.register(AccountGroup, AccountGroupAdmin)
 
